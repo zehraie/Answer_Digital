811903df9b11ff07e7112bf7af8981518dbaa796
zehraie <zehraie1976@gmail.com>
2022-02-17 22:38:33 +0000

    first commit-6

--- src/test/java/com/answer/pages/BasePage.java
+++ src/test/java/com/answer/pages/BasePage.java
@@ -1,12 +1,20 @@
 package com.answer.pages;
 
 import com.answer.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class BasePage {
+public abstract class BasePage {
 
+    @FindBy (linkText = "Form Authentication")
+    public WebElement formAuthent;
 
+    @FindBy(linkText = "Infinite Scroll")
+    public WebElement infinScroll;
 
+    @FindBy(linkText = "Key Presses")
+    public WebElement keyPress;
 
 
 
